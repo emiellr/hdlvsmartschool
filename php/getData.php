7341fa2fc7f34109788092cf839bdc41b13c632c
thomasio101 <thomasduursma@outlook.com>
2016-10-09 15:15:06 +0200

    Implement library methods in getData.php

--- php/getData.php
+++ php/getData.php
@@ -1,48 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "webapp";
-$password = "***";
-$database = "duursmait";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-  // Return failure
-  die("ERROR MYSQL connection error, details:" . $conn->connect_error . "(report to @thomasduursma on Slack)");
+function getArray($criteria=null, $table="datalog", $rows="*", $conn=null) {
+  // Get connection
+  $conn = $conn ? $conn:$GLOBALS["conn"];
+  
+  // Construct SQL
+  $sql = "SELECT $rows FROM $table";
+  $sql .= $criteria ? " WHERE $criteria":null;
+  
+  // Get data
+  $data = array();
+  $query = $conn->query($sql);
+  while($row = $query->fetch_assoc()) {
+    array_push($data, $row);
+  };
+  return $data;
 }
-
-// Prevent SQL injection
-if (strpos($_GET["criteria"] . $_GET["rows"], ";")) {
-  die("false");
-}
-
-// Construct sql
-$rows = "*";
-if($_GET["rows"]) {
-  $rows = join(", ", json_decode($_GET["rows"]));
-}
-
-$criteria = null;
-if($_GET["criteria"]) {
-  $criteria = " WHERE " . $_GET["criteria"];
-}
-
-$sql = "SELECT" . $rows . "FROM datalog" . $criteria;
-
-
-
-// Get data
-$query = $conn->query($sql);
-$result = array();
-
-while($row = $query->fetch_assoc()) {
-  array_push($result, $row);
-}
-
-// Close connection
-$conn->close();
-
-// Return data
-echo json_encode($result);
 ?>
